307b0b06fe5c184260ca4b37ff440c5ee988bbff
antonella <antonella.gutierrezrodriguez@pcc.edu>
2019-11-12 15:02:07 -0800

    things are working

--- src/components/controls/Controls.js
+++ src/components/controls/Controls.js
@@ -2,26 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Controls.css';
 
-const counter = (name, count) => {
-  if(name === 'DRINK_COFFEE') {
-    return (!!count.coffee && `- ${count.coffee}`);
-  }
-  else if(name === 'EAT_SNACK') {
-    return (!!count.snack && `- ${count.snack}`);
-  }
-  else if(name === 'TAKE_NAP') {
-    return (!!count.nap && `- ${count.nap}`);
-  }
-  else if(name === 'STUDY') {
-    return (!!count.study && `- ${count.study}`);
-  }
-};
-
 const Controls = ({ actions, handleSelection }) => (
   <section className={styles.Controls}>
     {actions.map(({ name, text, count }) => (
       <button key={name} onClick={() => handleSelection(name)}>
-        {text || name} {!!count && `${counter(name, count)}`}
+        {text || name} {!!count && `- ${count}`}
       </button>
     ))}
   </section>
--- src/containers/Moods.js
+++ src/containers/Moods.js
@@ -3,29 +3,21 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Controls from '../components/controls/Controls';
 import Face from '../components/face/Face';
+import { getActions, getFace } from '../selectors/moodSelectors';
 
-const actions = [
-  { name: 'DRINK_COFFEE', text: 'Drink Coffee', stateName: 'coffees' },
-  { name: 'EAT_SNACK', text: 'Snack', stateName: 'snacks' },
-  { name: 'TAKE_NAP', text: 'Nap', stateName: 'naps' },
-  { name: 'STUDY', text: 'Study', stateName: 'studies' },
-];
-
-export const isTired = state => state.coffees < 1 && state.naps < 1;
-export const isHyper = state => state.coffees > 3;
-export const isEducated = state => state.studies > 2;
-export const isHungry = state => state.snacks < 1;
-
-export const getFace = state => {
-  if(isTired(state) && isHungry(state)) return '🤬';
-  if(isHyper(state) && isHungry(state)) return '🤮';
-  if(isTired(state)) return '😴';
-  if(isHyper(state)) return '🙀';
-  if(isEducated(state)) return '🤯';
-  if(isHungry(state)) return '😡';
-
-  return '😀';
+const Moods = ({ actions, emoji, handleSelection }) => {
+  return (
+    <>
+      <Controls actions={actions} handleSelection={handleSelection} />
+      <Face emoji={emoji} />
+    </>
+  );
 };
+
+const mapStateToProps = state => ({
+  actions: getActions(state),
+  emoji: getFace(state)
+});
 
 const mapDispatchToProps = dispatch => ({
   handleSelection(name) {
@@ -35,45 +27,13 @@ const mapDispatchToProps = dispatch => ({
   }
 });
 
-const Moods = ({ face, coffees, snacks, naps, studies, handleSelection }) => {
-  const controlActions = actions.map(action => ({
-    ...action,
-    count: {
-      coffee: coffees,
-      snack: snacks,
-      nap: naps,
-      study: studies
-    }
-  }));
-
-  return (
-    <>
-      <Controls actions={controlActions} handleSelection={handleSelection}/>
-      <Face emoji={face} />
-    </>
-  );
-};
-
-const mapStateToProps = state => ({
-  coffees: state.coffees,
-  snacks: state.snacks,
-  naps: state.naps,
-  studies: state.studies,
-  face: getFace(state)
-});
-
-const MoodsContainer = connect(
+export default connect(
   mapStateToProps,
   mapDispatchToProps
 )(Moods);
 
 Moods.propTypes = {
-  coffees: PropTypes.number.isRequired,
-  snacks: PropTypes.number.isRequired,
-  naps: PropTypes.number.isRequired,
-  studies: PropTypes.number.isRequired,
-  face: PropTypes.string.isRequired,
+  actions: PropTypes.arrayOf(PropTypes.object).isRequired,
+  emoji: PropTypes.string.isRequired,
   handleSelection: PropTypes.func.isRequired
 };
-
-export default MoodsContainer;
